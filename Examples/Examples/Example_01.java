e97ddfc8a5b7569a0bea6b444bfd123d172eb05d
erosales48 <157036707+erosales48@users.noreply.github.com>
2024-06-25 21:24:59 -0400

    start usin IntelliJIdea

---
+++ Examples/Examples/Example_01.java
@@ -0,0 +1,16 @@
+/* Professor Darrell Payne
+ * Bellevue University
+ * 
+ * Application
+ */
+package Examples;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+public class Example_01 {
+
+}
